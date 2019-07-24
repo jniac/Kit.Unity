60560dbbba475ecd19e7eee3e5532f83ecb7c39e
jniac <joseph.merdrignac@gmail.com>
2019-07-24 14:34:21 +0200

    [add] Print(params...) (easier logging)

---
+++ Misc/Print.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Kit.Unity
+{
+    public static partial class Misc
+    {
+        public static void Print(params object[] args)
+        {
+            Debug.Log(string.Join(" ", args));
+        }
+    }
+}
