b6da8cbb1a8304dd9c0431a28952f76d7a34b05e
jniac <joseph.merdrignac@gmail.com>
2019-07-28 23:46:52 +0200

    ![add] Anim.To(target, duration, props)

---
+++ Anim/Ease.cs
@@ -0,0 +1,34 @@
+﻿namespace Kit.Unity
+{
+    using static UnityEngine.Mathf;
+
+    public partial class Anim
+    {
+        public static class Ease
+        {
+            public delegate float Del(float x);
+
+            public static Del Linear = x => x;
+
+            public static Del In(float power) => x => Pow(x, power);
+            public static Del In2 = x => x * x;
+            public static Del In3 = x => x * x * x;
+            public static Del In4 = x => x * x * x * x;
+            public static Del In5 = x => x * x * x * x * x;
+
+            public static Del Out(float power) => x => 1f - Pow(1f - x, power);
+            public static Del Out2 = x => 1f - (x = 1f - x) * x;
+            public static Del Out3 = x => 1f - (x = 1f - x) * x * x;
+            public static Del Out4 = x => 1f - (x = 1f - x) * x * x * x;
+            public static Del Out5 = x => 1f - (x = 1f - x) * x * x * x * x;
+
+            public static Del InOut(float power, float inflexion = .5f) => x => x < inflexion
+                ? Pow(inflexion, 1f - power) * Pow(x, power)
+                : 1f - Pow(1f - x, 1f - power) * Pow(1f - x, power);
+            public static Del InOut2 = x => x < .5f ? (x = x * 2f) * x / 2f : 1f - (x = 2f * (1f - x)) * x / 2f;
+            public static Del InOut3 = x => x < .5f ? (x = x * 2f) * x * x / 2f : 1f - (x = 2f * (1f - x)) * x * x / 2f;
+            public static Del InOut4 = x => x < .5f ? (x = x * 2f) * x * x * x / 2f : 1f - (x = 2f * (1f - x)) * x * x * x / 2f;
+            public static Del InOut5 = x => x < .5f ? (x = x * 2f) * x * x * x * x / 2f : 1f - (x = 2f * (1f - x)) * x * x * x * x / 2f;
+        }
+    }
+}
---
+++ Anim/To.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Kit.Utils;
+using UnityEngine;
+
+namespace Kit.Unity
+{
+    public partial class Anim
+    {
+        // Fetch the right method (name and params should match).
+        static MethodInfo Matching(MethodInfo[] methods, string name, Type paramType1, Type paramType2) =>
+            methods
+            .Where(m => m.Name == name)
+            .Single(m =>
+            {
+                var ps = m.GetParameters();
+
+                return ps.Length == 2
+                    && ps[0].ParameterType == paramType1
+                    && ps[1].ParameterType == paramType2;
+            });
+
+        static bool EvaluateDistance(object delta, out float distance)
+        {
+            if (delta is float)
+            {
+                distance = (float)delta;
+                return true;
+            }
+
+            if (delta is Vector3)
+            {
+                distance = ((Vector3)delta).magnitude;
+                return true;
+            }
+
+            distance = float.NaN;
+            return false;
+        }
+
+        public static Anim To(object target, float duration, object props, 
+            bool autoKillSimilarTarget = true, bool autoKillNullifiedTarget = true)
+        {
+            Type targetType = target.GetType();
+
+            List<Action<float>> actions = new List<Action<float>>();
+
+            Ease.Del ease = Ease.Linear;
+            float delay = 0;
+
+            // speed
+            float speed = float.NaN, distance = float.NaN;
+
+            foreach (var (property, index) in props.GetType().GetProperties().ItemIndex())
+            {
+                var type = property.PropertyType;
+                var name = property.Name;
+
+                if (name == "ease")
+                {
+                    ease = property.GetValue(props) as Ease.Del;
+                    continue;
+                }
+
+                if (name == "speed")
+                {
+                    speed = (float)property.GetValue(props);
+                    continue;
+                }
+
+                var propertyFrom = targetType.GetProperty(name);
+
+                bool propertyTypesMatch = propertyFrom.PropertyType == type;
+
+                if (!propertyTypesMatch)
+                    throw new Exception($"properties do not match: {name} {type} {propertyFrom.PropertyType}");
+
+                var methods = property.PropertyType.GetMethods();
+
+                var add = Matching(methods, "op_Addition", type, type);
+                var sub = Matching(methods, "op_Subtraction", type, type);
+                var mul = Matching(methods, "op_Multiply", type, typeof(float));
+
+                //// NOTE: May check for add, sub & mul existence
+
+                object from = propertyFrom.GetValue(target);
+                object to = property.GetValue(props);
+                object delta = sub.Invoke(null, new object[] { to, from });
+
+                if (EvaluateDistance(delta, out float distanceEvaluation))
+                    distance = distanceEvaluation;
+
+                Action<float> action = t => propertyFrom.SetValue(target,
+                    add.Invoke(null, new object[] { from, mul.Invoke(null, new object[] { delta, t }) }));
+
+                actions.Add(action);
+            }
+
+            if (!float.IsNaN(speed) && !float.IsNaN(distance))
+                duration = speed * distance;
+
+            if (autoKillSimilarTarget)
+                Kill(target);
+
+            return new Anim(target, anim => 
+            {
+                float t = ease(anim.Progress);
+
+                foreach (var action in actions)
+                    action(t);
+
+            }, duration, delay, autoKillNullifiedTarget);
+        }
+
+        public static Anim To(object target, object props,
+            bool autoKillSimilarTarget = true, bool autoKillNullifiedTarget = true) =>
+            To(target, 1, props, autoKillSimilarTarget, autoKillNullifiedTarget);
+
+    }
+}
