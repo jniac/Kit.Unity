a11b27f529f3a5e7c368617bcdb8ad3b5fd630ad
jniac <joseph.merdrignac@gmail.com>
2019-09-06 10:11:47 +0200

    [add] Misc.IfChanged(old, new, ...callbacks)

---
+++ Misc/Utils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Kit.Unity
+{
+    public static partial class Misc
+    {
+        public static void IfChanged<T>(T oldValue, T newValue, params Action<T>[] callbacks)
+        {
+            if (!oldValue.Equals(newValue))
+                foreach(var callback in callbacks)
+                    callback(newValue);
+        }
+    }
+}
