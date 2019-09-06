1ea1ce9b9833fd6f1f8b3992192affbd8f13dfdb
jniac <joseph.merdrignac@gmail.com>
2019-09-06 19:13:10 +0200

    [up] Vector3 extensions

--- Vector3Extensions.cs
+++ Vector3Extensions.cs
@@ -5,6 +5,14 @@ namespace Kit.Unity
 {
     public static class Vector3Extensions
     {
+        public static Vector2 XY(this Vector3 v) => new Vector2(v.x, v.y);
+        public static Vector2 XY(this Vector3 v, float x, float y) => new Vector2(v.x * x, v.y * y);
+
         public static Vector2 XZ(this Vector3 v) => new Vector2(v.x, v.z);
+
+        public static Vector2 YZ(this Vector3 v) => new Vector2(v.y, v.z);
+
+        public static Vector2 ZY(this Vector3 v) => new Vector2(v.z, v.y);
+        public static Vector2 ZY(this Vector3 v, float z, float y) => new Vector2(v.z * z, v.y * y);
     }
 }
