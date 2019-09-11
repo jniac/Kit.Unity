0c75279b8d2950065eda07b25999f52bdacac538
jniac <joseph.merdrignac@gmail.com>
2019-09-11 16:35:57 +0200

    [up] Ticker: ExecuteInEditMode is buggy, try to fix

--- Anim/Ticker.cs
+++ Anim/Ticker.cs
@@ -1,42 +1,106 @@
 ﻿using System;
+using System.Collections;
+using System.Threading.Tasks;
 using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Kit.Unity
 {
     public partial class Anim
     {
-        static Ticker ticker;
-
+        /*
+         * ExecuteInEditMode is very buggy.
+         * The class seems to work, but let's be careful.
+         */
         [ExecuteInEditMode]
-        class Ticker : MonoBehaviour
+        public class Ticker : MonoBehaviour
         {
+            public static Ticker Instance { get; private set; }
+
             public static void Init()
             {
-                if (!ticker)
+                if (!Instance)
                 {
-                    ticker = FindObjectOfType<Ticker>();
+                    Instance = FindObjectOfType<Ticker>();
 
-                    if (!ticker)
+                    if (!Instance)
                     {
-                        GameObject gameObject = GameObject.Find("Main") 
-                            ?? new GameObject($"{typeof(Anim).Name}-{typeof(Ticker).Name}");
-
+                        GameObject gameObject = new GameObject($"{typeof(Anim).Name}-{typeof(Ticker).Name}");
                         gameObject.hideFlags = HideFlags.HideAndDontSave;
-
-                        ticker = gameObject.AddComponent<Ticker>();
+                        gameObject.AddComponent<Ticker>();
                     }
                 }
             }
 
+            static int count;
+            public readonly int instanceId = count++;
+
+            bool disabled;
+
             private void OnEnable()
             {
-                print(this);
+                //(instanceId, GetInstanceID(), hideFlags).Print("OnEnable");
+
+                disabled = false;
+
+                if (instanceId == 0)
+                {
+                    Instance = this;
+
+                    if (!Application.isPlaying)
+                        print($"well we are in the editor, Anim will not work very well {GetInstanceID()}");
+                }
+            }
+
+            private void OnDisable()
+            {
+                //(instanceId, GetInstanceID(), hideFlags).Print("OnDisable");
+
+                disabled = true;
+            }
+
+            private void OnDestroy()
+            {
+                if (instanceId == 0)
+                {
+                    // reset
+                    Instance = null;
+                    count = 0;
+
+                    print("Anim Instance 0 destroyed");
+                }
             }
 
             private void Update()
             {
-                UpdateAll(Time.deltaTime);
+                if (instanceId == 0)
+                {
+                    if (Application.isPlaying)
+                        UpdateAll(Time.deltaTime);
+                }
+                else
+                {
+                    // security check, destroy any duplicate
+                    DestroyImmediate(gameObject);
+                }
             }
+
+#if UNITY_EDITOR
+            [MenuItem("Tools/Kit/Init Anim (debug)")]
+            static void InitAnim()
+            {
+                Init();
+            }
+            [MenuItem("Tools/Kit/Reset Anim (debug")]
+            static void ResetAnim()
+            {
+                if (Instance)
+                    DestroyImmediate(Instance.gameObject);
+            }
+#endif
         }
     }
 }
