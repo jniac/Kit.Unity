24e818a414f60f9f9cceebe41d939dd6139235c5
jniac <joseph.merdrignac@gmail.com>
2019-09-13 13:16:36 +0200

    [add] Misc.HelpBox (Editor)

---
+++ Misc/Editor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Kit.Utils;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Kit.Unity
+{
+    public static partial class Misc
+    {
+#if UNITY_EDITOR
+        public static void HelpBox(MessageType type, params object[] args) =>
+            EditorGUILayout.HelpBox(args.StringJoin("\n"), type);
+
+        public static void HelpBox(params object[] args) =>
+            HelpBox(MessageType.None, args);
+#endif
+    }
+}
