2ff08186151b6a942083eda087d338eec92d667f
jniac <joseph.merdrignac@gmail.com>
2019-09-16 13:03:47 +0200

    [add] Misc.WithGuiDisabled = () => {...}

---
+++ Misc/GUI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Kit.Unity
+{
+    public static partial class Misc
+    {
+        public static Action WithGuiDisabled
+        {
+            set 
+            {
+                bool before = GUI.enabled;
+                GUI.enabled = false;
+                value();
+                GUI.enabled = before;
+            }
+        }
+    }
+}
