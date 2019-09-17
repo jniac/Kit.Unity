9e391137f6ce3203318a27c9c1918513cb0bb06c
jniac <joseph.merdrignac@gmail.com>
2019-09-17 15:42:16 +0200

    [add] Misc.WithGui(bool, action)

--- Misc/GUI.cs
+++ Misc/GUI.cs
@@ -5,6 +5,14 @@ namespace Kit.Unity
 {
     public static partial class Misc
     {
+        public static void WithGui(bool enabled, Action action)
+        {
+            bool before = GUI.enabled;
+            GUI.enabled = enabled;
+            action();
+            GUI.enabled = before;
+        }
+
         public static Action WithGuiDisabled
         {
             set 
