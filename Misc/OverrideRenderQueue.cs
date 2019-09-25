fd3862ad1edd675d286f290334a6b93c2208f4cf
jniac <joseph.merdrignac@gmail.com>
2019-09-25 18:08:53 +0200

    [add] OverrideRenderQueue

---
+++ Misc/OverrideRenderQueue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using System.Linq;
+using Kit.Utils;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Kit.Unity
+{
+    [ExecuteAlways]
+    public class OverrideRenderQueue : MonoBehaviour
+    {
+        public bool overrideRenderQueue;
+        public int renderQueue;
+
+        private void OnValidate()
+        {
+            if (overrideRenderQueue)
+            {
+                foreach(var m in GetComponent<MeshRenderer>().sharedMaterials)
+                {
+                    m.renderQueue = renderQueue;
+                }
+            }
+        }
+#if UNITY_EDITOR
+        [CustomEditor(typeof(OverrideRenderQueue))]
+        class MyEditor : Editor
+        {
+            OverrideRenderQueue Target => target as OverrideRenderQueue;
+
+            public override void OnInspectorGUI()
+            {
+                base.OnInspectorGUI();
+
+                if (!Target.overrideRenderQueue)
+                    Misc.HelpBox(
+                        "Current render queue:",
+                        Target.GetComponent<MeshRenderer>().sharedMaterials.Select(m => m.renderQueue).StringJoin(),
+                        null);
+
+                if (GUILayout.Button("Remove"))
+                    Target.gameObject.RemoveComponent<OverrideRenderQueue>();
+            }
+        }
+#endif
+    }
+}
