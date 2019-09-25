814f3dcd00af0bcb695a19e301983a423288af96
jniac <joseph.merdrignac@gmail.com>
2019-09-25 18:08:25 +0200

    [add] MaterialInspector

---
+++ MaterialInspector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using System.Linq;
+using Kit.Utils;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Kit.Unity
+{
+    public class MaterialInspector : MonoBehaviour
+    {
+
+
+#if UNITY_EDITOR
+        [CustomEditor(typeof(MaterialInspector))]
+        public class MyEditor : Editor
+        {
+            MaterialInspector Target => target as MaterialInspector;
+
+            public override void OnInspectorGUI()
+            {
+                var materials = Target.GetComponent<MeshRenderer>().sharedMaterials;
+
+                Misc.HelpBox(
+                    $"materials({materials.Length})",
+                    materials.Select((m, i) => $"{i}: Render Queue: {m.shader.renderQueue}").StringJoin("\n"),
+                    null);
+            }
+        }
+#endif
+    }
+}
