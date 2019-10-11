136d448625ec6c3079d20af0b4dffe74631f728d
jniac <joseph.merdrignac@gmail.com>
2019-10-11 12:54:36 +0200

    [add] ext: Vector2.GetArea(), Vector3.AreaIsNull()

--- Vector2Extensions.cs
+++ Vector2Extensions.cs
@@ -5,6 +5,9 @@ namespace Kit.Unity
 {
     public static class Vector2Extensions
     {
+        public static float GetArea(this Vector2 v) =>
+            v.x * v.y;
+
         public static float GetAngle(this Vector2 v) =>
             Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
     }
--- Vector3Extensions.cs
+++ Vector3Extensions.cs
@@ -5,6 +5,15 @@ namespace Kit.Unity
 {
     public static class Vector3Extensions
     {
+        // https://stackoverflow.com/a/3076081/4696005
+        public static bool AreaIsNull(this Vector3 v)
+        {
+            bool yz = false, zx = false;
+            return (Mathf.Abs(v.x * v.y) < Mathf.Epsilon)
+                && ((yz = Mathf.Abs(v.y * v.z) < Mathf.Epsilon) || (zx = Mathf.Abs(v.z * v.x) < Mathf.Epsilon))
+                || (yz && zx);
+        }
+
         public static Vector2 XY(this Vector3 v) => new Vector2(v.x, v.y);
         public static Vector2 XY(this Vector3 v, float x, float y) => new Vector2(v.x * x, v.y * y);
 
