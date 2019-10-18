23fb1998078ba880e70bc579097956f61934be32
jniac <joseph.merdrignac@gmail.com>
2019-10-18 11:00:30 +0200

    [add] Misc.WithX()

---
+++ Misc/Vector3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Kit.Unity
+{
+    public static partial class Misc
+    {
+        public static Vector3 WithX(Vector3 v, float x)
+        {
+            return new Vector3(x, v.y, v.z);
+        }
+
+        public static Vector3 WithX(ref Vector3 v, float x)
+        {
+            v.x = x;
+
+            return v;
+        }
+    }
+}
