05aeb4a13e683f23da5068e2dcec490d0b406d11
jniac <joseph.merdrignac@gmail.com>
2019-10-21 14:33:33 +0200

    [add] WithZ

--- Misc/Vector3.cs
+++ Misc/Vector3.cs
@@ -5,10 +5,11 @@ namespace Kit.Unity
 {
     public static partial class Misc
     {
-        public static Vector3 WithX(Vector3 v, float x)
-        {
-            return new Vector3(x, v.y, v.z);
-        }
+        public static Vector3 WithX(Vector3 v, float x) =>
+            new Vector3(x, v.y, v.z);
+
+        public static Vector3 WithZ(Vector3 v, float z) =>
+            new Vector3(v.x, v.y, z);
 
         public static Vector3 WithX(ref Vector3 v, float x)
         {
