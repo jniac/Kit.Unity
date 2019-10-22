cfc7a7c1ed4fa97a80784c50dc10736ba4fac73c
jniac <joseph.merdrignac@gmail.com>
2019-10-22 16:40:18 +0200

    [add] Color ext: Color.WithAlpha() (better late than never)

---
+++ Extensions/Color.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Kit.Unity
+{
+    public static class ColorExtensions
+    {
+        public static Color WithAlpha(this Color color, float alpha) => 
+            new Color(color.r, color.g, color.b, alpha);
+    }
+}
