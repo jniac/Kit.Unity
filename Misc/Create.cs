c2632966a4ef60a64d811d6ac4412b74728bbf4e
jniac <joseph.merdrignac@gmail.com>
2019-10-24 02:58:39 +0200

    [add] Misc.Create<T>

---
+++ Misc/Create.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Kit
+{
+    public static partial class Misc
+    {
+        public static T Create<T>(Transform parent, string name = null) 
+            where T : Component
+        {
+            var go = new GameObject(name);
+
+            go.transform.SetParent(parent, false);
+
+            return go.AddComponent<T>();
+        }
+    }
+}
