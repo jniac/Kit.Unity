61814027296990afcfcf9460d259058254963f41
jniac <joseph.merdrignac@gmail.com>
2019-10-27 11:01:07 +0100

    [up] better OverriideRenderQueue (clone Material)

--- Misc/OverrideRenderQueue.cs
+++ Misc/OverrideRenderQueue.cs
@@ -33,13 +33,16 @@ namespace Kit
 
         private void OnEnable()
         {
-            mr.sharedMaterial = clone;
+            mr.sharedMaterial = overrideRenderQueue ? clone : original;
         }
 
         void OnValidate()
         {
-            if (clone && overrideRenderQueue)
+            if (clone)
+            {
+                mr.sharedMaterial = overrideRenderQueue ? clone : original;
                 clone.renderQueue = renderQueue;
+            }
         }
 
 #if UNITY_EDITOR
@@ -50,13 +53,34 @@ namespace Kit
 
             public override void OnInspectorGUI()
             {
-                base.OnInspectorGUI();
+                EditorGUI.BeginChangeCheck();
+
+                Target.overrideRenderQueue =
+                    EditorGUILayout.Toggle("Override", Target.overrideRenderQueue);
+
+                GUI.enabled = Target.overrideRenderQueue;
 
                 if (!Target.overrideRenderQueue)
-                    Misc.HelpBox(
-                        "Current render queue:",
-                        Target.GetComponent<MeshRenderer>().sharedMaterials.Select(m => m.renderQueue).StringJoin(),
-                        null);
+                {
+                    var actual = Target.GetComponent<MeshRenderer>().sharedMaterials.Select(m => m.renderQueue).StringJoin();
+                    EditorGUILayout.TextField("Render Queue", actual);
+                }
+                else
+                {
+                    Target.renderQueue =
+                        EditorGUILayout.IntField("Render Queue", Target.renderQueue);
+                }
+
+                //base.OnInspectorGUI();
+
+                //if (!Target.overrideRenderQueue)
+                Misc.HelpBox(
+                "Current render queue:",
+                Target.GetComponent<MeshRenderer>().sharedMaterials.Select(m => m.renderQueue).StringJoin(),
+                null);
+
+                if (EditorGUI.EndChangeCheck())
+                    Target.OnValidate();
 
                 if (GUILayout.Button("Remove"))
                     Target.gameObject.RemoveComponent<OverrideRenderQueue>();
