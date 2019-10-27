be6c773db7aaeeca1f57efd164f0dece96a1c839
jniac <joseph.merdrignac@gmail.com>
2019-10-27 11:01:35 +0100

    [up] better Transform.First<?>() (throw readable errors)

--- TransformGet.cs
+++ TransformGet.cs
@@ -37,37 +37,65 @@ namespace Kit
             name => mask[0] == '!' ? name != mask.Substring(1) : name == mask;
 
         public static IEnumerable<T> Get<T>(this Transform transform,
-            string str, bool includeSelf = false, int recursiveLimit = -1)
+            string query, bool includeSelf = false, int recursiveLimit = -1)
             where T : Component
         {
-            if (str.Contains(','))
+            if (query.Contains(','))
             {
-                var parts = new Regex(@"\s*,\s*").Split(str);
+                var parts = new Regex(@"\s*,\s*").Split(query);
 
                 return Get<T>(transform, t => parts.Contains(t.gameObject.name), includeSelf, recursiveLimit);
             }
 
-            if (str[0] == '!')
+            if (query[0] == '!')
             {
-                str = str.Substring(1);
-                return Get<T>(transform, t => t.gameObject.name != str, includeSelf, recursiveLimit);
+                query = query.Substring(1);
+                return Get<T>(transform, t => t.gameObject.name != query, includeSelf, recursiveLimit);
             }
 
-            return Get<T>(transform, t => t.gameObject.name == str, includeSelf, recursiveLimit);
+            return Get<T>(transform, t => t.gameObject.name == query, includeSelf, recursiveLimit);
+        }
+
+        public static IEnumerable<Transform> Get(this Transform transform,
+            string query, bool includeSelf = false, int recursiveLimit = -1) =>
+            Get<Transform>(transform, query, includeSelf, recursiveLimit);
+
+
+
+
+
+        public static T First<T>(this Transform transform,
+            Func<Transform, bool> test = null, bool includeSelf = false, int recursiveLimit = -1, bool throwError = true)
+            where T : Component
+        {
+            var result = Get<T>(transform, test, includeSelf, recursiveLimit);
+
+            if (result.Any())
+                return result.First();
+
+            if (throwError)
+                throw new Exception($"oups, no child match \"{test}\"");
+
+            return null;
         }
 
         public static T First<T>(this Transform transform,
-            Func<Transform, bool> test = null, bool includeSelf = false, int recursiveLimit = -1)
-            where T : Component =>
-            Get<T>(transform, test, includeSelf, recursiveLimit).FirstOrDefault();
+            string query, bool includeSelf = false, int recursiveLimit = -1, bool throwError = true)
+            where T : Component
+        {
+            var result = Get<T>(transform, query, includeSelf, recursiveLimit);
 
-        public static T First<T>(this Transform transform,
-            string gameObjectName, bool includeSelf = false, int recursiveLimit = -1)
-            where T : Component =>
-            Get<T>(transform, gameObjectName, includeSelf, recursiveLimit).FirstOrDefault();
+            if (result.Any())
+                return result.First();
+
+            if (throwError)
+                throw new Exception($"oups, no child match \"{query}\"");
+
+            return null;
+        }
 
         public static Transform First(this Transform transform,
-            string gameObjectName, bool includeSelf = false, int recursiveLimit = -1) =>
-            Get<Transform>(transform, gameObjectName, includeSelf, recursiveLimit).FirstOrDefault();
+            string query, bool includeSelf = false, int recursiveLimit = -1, bool throwError = true) =>
+            First<Transform>(transform, query, includeSelf, recursiveLimit, throwError);
     }
 }
